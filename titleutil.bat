062cabaa27fba0c3d4029fcf362cb3d5e70f6837
Baebird <dapperwbs@gmail.com>
2023-06-17 15:37:47 -0400

    Accidentally was dumb
    Didn't add stupid venv folder and didn't remove dumb me specific things
    so whatever

--- titleutil.bat
+++
@@ -1,4 +0,0 @@
-call "C:\Users\reese\OneDrive\Desktop\MKBashes\Title Screen\venv\Scripts\activate.bat"
-call "C:\Users\reese\OneDrive\Desktop\MKBashes\Title Screen\venv\Scripts\python.exe" "C:\Users\reese\OneDrive\Desktop\MKBashes\Title Screen\p_titleutil.py" %1 %2 %3 %4 %5 %6
-call "C:\Users\reese\OneDrive\Desktop\MKBashes\Title Screen\venv\Scripts\deactivate.bat"
-@echo off
